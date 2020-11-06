155e25f2ae5f506e80cdc4523ca1fc9590857884
davidpalau <palau.david@hotmail.com>
2020-11-06 20:42:48 +0100

    funciones javaScipt

---
+++ funciones/fuction.js
@@ -0,0 +1,44 @@
+/*Prácticas de Funciones
+Las prácticas se entregan indicando en la tarea la URL donde encontrar el desarrollo en GitHub.
+Es obligatorio hacer las prácticas con el entorno de desarrollo que se ha definido al inicio del curso.
+Si en la práctica se pide crear una aplicación web, añadiremos la entrada a la página home de nuestra aplicación (index.html), de tal forma que se pueda navegar desde esta página a cada una de las prácticas que hacemos.
+
+Práctica 1 (Obligatoria)
+
+Crea una aplicación web que pida al usuario palabras continuamente hasta que que el popup se deje vacío o se cancele.
+
+A continuación se eliminarán todas las palabras repetidas y además se ordenarán en español, pero en orden inverso (de la Z a la A) y se mostrará el resultado por pantalla.
+
+Utilizar funciones flecha, si se considera apropiado. */
+
+/*
+
+Práctica 2 (Obligatoria)
+
+
+Crea una aplicación web que pida al usuario palabras continuamente hasta que que el popup se deje vacío o se cancele.
+
+Crear una función que recibirá todas las palabras y volverá un mapa que contenga como clave cada palabra y el valor será el número de veces que aparece esa palabra en el array. Mostrar resultados por pantalla.
+
+Se valorará positivamente si también se hacen los tests unitarios apropiados de la función que se ha desarrollado mediante Jester.
+
+
+
+Práctica 3 (Opcional)
+
+El método filter los arrays permite indicar una función callback, para aplicar un filtro a los elementos del array.
+
+Es una buena práctica para aprender a implementar funciones callback tratar de crear nuestras propias funciones.
+
+Crea una función que se diga filtro y que reciba un array y una función callback. La función callback entenderá que sólo tiene un parámetro. Esta función se aplicará a cada elemento del array y el resultado se volverá a guardar en el mismo array.
+
+Por ejemplo: Array [1,2,3,4]
+
+función = multiplicar por 2
+
+Resultado: [2, 4, 6, 8]
+
+Crear otros dos casos:
+
+Dado un array de palabras en minúsculas, pasarlo a mayúsculas
+Dado un array de números, calcular el factorial de cada uno (https://es.wikipedia.org/wiki/Factorial) */
